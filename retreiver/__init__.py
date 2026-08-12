080e9f888a777f3ca11e941aa4e6e9eda5303221
Савинский Владимир <vovka_savinskiy@mail.ru>
2026-08-12 16:09:23 +0300

    fix: исправлен поиск по базе

--- retreiver/__init__.py
+++ retreiver/__init__.py
@@ -2,17 +2,24 @@ from langchain_chroma import Chroma
 from langchain_ollama import OllamaEmbeddings
 
 
+DB_PATH = "chroma_db"
+COLLECTION_NAME = "pdf_documents"
+OLLAMA_URL = "http://127.0.0.1:11434"
+EMBEDDING_MODEL = "nomic-embed-text"
+
+
 embeddings = OllamaEmbeddings(
-    model="nomic-embed-text"
+    model=EMBEDDING_MODEL,
+    base_url=OLLAMA_URL,
 )
 
 vectorstore = Chroma(
-    persist_directory="chroma_db",
-    embedding_function=embeddings
+    collection_name=COLLECTION_NAME,
+    persist_directory=DB_PATH,
+    embedding_function=embeddings,
 )
 
 retriever = vectorstore.as_retriever(
-    search_kwargs={
-        "k": 4
-    }
+    search_type="similarity",
+    search_kwargs={"k": 4},
 )
